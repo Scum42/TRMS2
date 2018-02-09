709767380cb1c5c2420227c43137333902a307b8
nicholas birchfield <nbirchfield64@gmail.com>
2018-02-09 13:28:24 -0500

    all necessary tables???

--- SQL/TRMS2_reset.sql
+++ SQL/TRMS2_reset.sql
@@ -18,46 +18,92 @@ grant create view to trms2;
 -- Connect to the user we just made
 conn trms2/p4ssw0rd;
 
-create sequence users_pk_seq;
+drop table tournament cascade constraints;
+drop table tourny_registry cascade constraints;
+drop table tourny_rounds cascade constraints;
+drop table login cascade constraints;
+
+drop sequence tournyid_seq;
+drop sequence register_seq;
+drop sequence rounds_seq;
+drop sequence users_pk_seq;
+drop sequence round_pk_seq;
+drop sequence result_pk_seq;
+drop sequence tourny_user_pk_seq;
+drop sequence tourny_round_pk_seq;
+
 create table users
 (
-    id number(20) primary key,
+    user_id number(20) primary key,
     firstname varchar2(32),
     lastname varchar2(32),
     username varchar2(32) unique not null,
+    pass varchar2(32) not null,
     email varchar2(32) unique not null
-)
+);
+--
+create table team (
+    team_id number(10) primary key,
+    team_name varchar2(256) unique not null,
+    captain number(10) not null
+);
 
+--
+create table team_to_user (
+    record_id number(10) primary key,
+    t_id number(10) not null,
+    p_id number(10) not null
+);
+--
 create table tournament (
     tournament_id number(10) primary key,
-    type varchar2(256) not null,
-    style varchar(256) not null, --Will be bracket only for now
+    tuourny_type varchar2(256) not null,
+    style varchar2(256) not null, --Will be bracket only for now
     owner_id number(10) not null,
     judge_id number(10) not null,
     min_num number(10) not null,
     max_num number(10) not null
 );
-
-create table tourny_registry (
+--
+create table tourny_to_user (
     record_id number(10) primary key,
     participant_id number(10) not null,
     tourny_id number(10) not null
 );
-
-create table tourny_rounds (
+--
+create table tourny_to_round (
     record_id number(10) primary key,
+    round_id number(10) not null,
+    tourny_id number(10) not null
+);
+--
+create table round (
+    round_id number(10) primary key,
     round_num number(10) not null,
-    tourny_id number(10) not null,
-    player_1 number(10), -- Allow to be null or have a default value
-    player_2 number(10), -- same as above
-    team_1 number(10), -- same as above
-    team_2 number(10), -- same as above
-    win number(10) not null, -- default value for pending results
-    loss number(10) not null
+    player1 number(10) not null,
+    player2 number(10) not null,
+    r_result number(10) not null
+);
+--
+create table results (
+    result_id number(10) primary key,
+    win number(10), -- player id of winner
+    loss number(10) -- player id of loser
 );
 
 -------- Constraints --------
-/*
+alter table team_to_user
+add constraint fk_t_id foreign key (t_id)
+references team (team_id) on delete cascade;
+
+alter table team_to_user
+add constraint fk_p_id foreign key (p_id)
+references users (user_id) on delete cascade;
+
+alter table team
+add constraint fk_captain foreign key (captain)
+references users (user_id) on delete cascade;
+
 alter table tournament
 add constraint fk_owner_id foreign key (owner_id)
 references users (user_id) on delete cascade;
@@ -66,28 +112,49 @@ alter table tournament
 add constraint fk_judge_id foreign key (judge_id)
 references users (user_id) on delete cascade;
 
-alter table tourny_registry
+alter table tourny_to_round
 add constraint fk_tourny_id foreign key (tourny_id)
 references tournament (tournament_id) on delete cascade;
 
-alter table tourny_registry
+alter table tourny_to_round
+add constraint fk_round_id foreign key (round_id)
+references round (round_id) on delete cascade;
+
+alter table round
+add constraint fk_player1 foreign key (player1)
+references users (user_id) on delete cascade;
+
+alter table round
+add constraint fk_player2 foreign key (player2)
+references users (user_id) on delete cascade;
+
+alter table round
+add constraint fk_result foreign key (r_result)
+references results (result_id) on delete cascade;
+
+alter table tourny_to_user
 add constraint fk_participant_id foreign key (participant_id)
 references users (user_id) on delete cascade;
 
-alter table tourny_rounds
-add constraint fk_tourny_id foreign key (tourny_id)
+alter table tourny_to_user
+add constraint fk_tourny_user_id foreign key (tourny_id)
 references tournament (tournament_id) on delete cascade;
 
-alter table tourny_rounds
-add constraint fk_player1 foreign key (player_1)
+alter table results
+add constraint fk_win foreign key (win)
 references users (user_id) on delete cascade;
 
-alter table tourny_rounds
-add constraint fk_player2 foreign key (player_2)
+alter table results
+add constraint fk_loss foreign key (loss)
 references users (user_id) on delete cascade;
-*/
+
 
 ------ Sequences for Hibernate -------
 create sequence tournyid_seq;
 create sequence register_seq;
 create sequence rounds_seq;
+create sequence users_pk_seq;
+create sequence round_pk_seq;
+create sequence result_pk_seq;
+create sequence tourny_user_pk_seq;
+create sequence tourny_round_pk_seq;
