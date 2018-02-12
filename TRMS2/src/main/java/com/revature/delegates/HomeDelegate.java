534cd8ff9051427dd4aa878ca45f2192b9aadd13
Anthony Libardi <libardi42@gmail.com>
2018-02-12 09:59:19 -0500

    SERVLET?!

--- TRMS2/src/main/java/com/revature/delegates/HomeDelegate.java
+++ TRMS2/src/main/java/com/revature/delegates/HomeDelegate.java
@@ -6,10 +6,20 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.log4j.Logger;
+import org.hibernate.Session;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.revature.beans.User;
 import com.revature.controller.Delegate;
+import com.revature.util.HibernateUtil;
 
 public class HomeDelegate extends Delegate
 {
+    private static Logger log = Logger.getLogger(HomeDelegate.class);
+    private static HibernateUtil hu = HibernateUtil.getInstance();
+    private static ObjectMapper om = new ObjectMapper();
+    
     public HomeDelegate(String uri)
     {
         super(uri);
@@ -18,6 +28,9 @@ public class HomeDelegate extends Delegate
     @Override
     public void processGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
     {
-        req.getRequestDispatcher("static/home.html").forward(req, resp);
+        Session s = hu.getSession();
+        User u;
+        u = s.get(User.class, 1);
+        resp.getWriter().write(om.writeValueAsString(u));
     }
 }
