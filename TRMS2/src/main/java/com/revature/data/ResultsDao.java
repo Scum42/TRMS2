3bb6d15a20b839f6094821535d767d9fafc5a317
Dillon Euell Pape <dillonpape@gmail.com>
2018-02-13 01:36:31 -0500

    Some source formatting and Javadoc-ing

--- TRMS2/src/main/java/com/revature/data/ResultsDao.java
+++ TRMS2/src/main/java/com/revature/data/ResultsDao.java
@@ -4,34 +4,37 @@ import com.revature.beans.Results;
 
 public interface ResultsDao {
 
-	/*
+	/**
 	 * Inserts Results object in database
 	 * 
-	 * input: result -- Result object to be inserted
-	 * return: id of created Results
+	 * @param result
+	 *            Result object to be inserted
+	 * @return id of created Results
 	 */
 	public Results persistResults(Results result);
-	
-	/*
+
+	/**
 	 * returns Results object from database
 	 * 
-	 * input: id -- identifier of Results object to get
-	 * return: Results object that matches id, else null
+	 * @param id
+	 *            identifier of Results object to get
+	 * @return Results object that matches id, else null
 	 */
 	public Results loadResults(int id);
-	
-	/*
+
+	/**
 	 * deletes Results object from dataabse
 	 * 
-	 * input: result -- Results object to delete
-	 *  
+	 * @param result
+	 *            Results object to delete
 	 */
 	public void deleteResults(Results result);
-	
-	/*
+
+	/**
 	 * updates Results object in database
 	 * 
-	 * input: result -- Results object to be updated
+	 * @param result
+	 *            Results object to be updated
 	 */
 	public Results merge(Results result);
 }
--- TRMS2/src/main/java/com/revature/data/TournamentDao.java
+++ TRMS2/src/main/java/com/revature/data/TournamentDao.java
@@ -6,18 +6,19 @@ import com.revature.beans.Tournament;
 import com.revature.beans.User;
 
 public interface TournamentDao {
-	
+
 	public Tournament persistTournament(Tournament tournament);
-	
+
 	public Tournament loadTournament(int id);
-	
+
 	public List<Tournament> loadTournamentsByOwner(User user);
-	
+
 	public List<Tournament> loadTournamentsByJudge(User user);
-	
+
 	public List<Tournament> loadTournamentsByPlayer(User user);
-	
+
 	public void deleteTournament(Tournament tournament);
-	
+
 	public void mergeTournament(Tournament tournament);
+
 }
