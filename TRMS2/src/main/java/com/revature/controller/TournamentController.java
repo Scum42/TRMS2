8c7ab53c394eee3c72f22a4b4cb50307a191894f
Anthony Libardi <libardi42@gmail.com>
2018-02-13 20:33:47 -0500

    TournamentController hu fix

--- TRMS2/src/main/java/com/revature/controller/TournamentController.java
+++ TRMS2/src/main/java/com/revature/controller/TournamentController.java
@@ -11,24 +11,29 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.revature.beans.Tournament;
-import com.revature.util.HibernateUtil;
+import com.revature.data.HibernateSession;
 
 @Controller
 @RequestMapping(value = "/tournament")
 @CrossOrigin(origins = { "http://localhost:4200", "http://18.216.71.226:4200" })
-public class TournamentController {
+public class TournamentController implements HibernateSession {
 	private static Logger log = Logger.getLogger(TournamentController.class);
-	private static HibernateUtil hu = HibernateUtil.getInstance();
 	private static ObjectMapper om = new ObjectMapper();
+
+	private Session session;
 
 	@RequestMapping(method = RequestMethod.GET)
 	@ResponseBody
 	public String getTournament() throws JsonProcessingException {
-		Session s = hu.getSession();
-		Tournament t = (Tournament) s.get(Tournament.class, 1);
+		Tournament t = (Tournament) session.get(Tournament.class, 1);
 		String str = om.writeValueAsString(t);
 		log.trace(str);
-		s.close();
+		session.close();
 		return om.writeValueAsString(t);
+	}
+
+	@Override
+	public void setSession(Session session) {
+		this.session = session;
 	}
 }
