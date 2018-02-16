26a2b1ffe001ef91914b67cdb89fb59b1c8cae6d
Anthony Libardi <libardi42@gmail.com>
2018-02-16 11:31:37 -0500

    Register user is almost done

--- TRMS2/src/main/java/com/revature/controller/LoginController.java
+++ TRMS2/src/main/java/com/revature/controller/LoginController.java
@@ -12,48 +12,59 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.revature.beans.User;
 import com.revature.data.UserDao;
-import com.revature.util.JsonResult;
+import com.revature.util.JsonUtil;
 
 @Controller
 @CrossOrigin(origins = { "http://localhost:4200", "http://18.216.71.226:4200" })
 public class LoginController {
 	private static Logger log = Logger.getLogger(LoginController.class);
-	private static ObjectMapper om = new ObjectMapper();
 
 	@Autowired
 	UserDao udao;
+
+	@Autowired
+	JsonUtil ju;
 
 	@RequestMapping(value = "/login", method = RequestMethod.POST)
 	@ResponseBody
 	public String login(@RequestBody User u, HttpSession httpSession) throws JsonProcessingException {
 		User current = udao.loadUserByUsernameAndPassword(u.getUsername(), u.getPassword());
 		httpSession.setAttribute("user", current);
-		String json = om.writeValueAsString(current);
+		String json = ju.toJson(current);
 		return json;
 	}
 
 	@RequestMapping(value = "/loggedIn", method = RequestMethod.GET)
 	@ResponseBody
-	public String getLoggedInUser(HttpSession session) throws JsonProcessingException {
-		User u = (User) session.getAttribute("user");
-		String json = om.writeValueAsString(u);
+	public String getLoggedInUser(HttpSession httpSession) throws JsonProcessingException {
+		User u = (User) httpSession.getAttribute("user");
+		log.trace("Get logged in user got " + u + " from the session");
+		String json = ju.toJson(u);
+		log.trace(json == null ? "JSON was null" : "JSON wasn't null and was \"" + json + "\"");
 		return json;
 	}
 
 	@RequestMapping(value = "/register", method = RequestMethod.POST)
 	@ResponseBody
-	public String registerUser(@RequestBody User u, HttpSession httpSession) {
-		udao.persistUser(u);
-		return JsonResult.getResult(true);
+	public String registerUser(@RequestBody User u, HttpSession httpSession) throws JsonProcessingException {
+		try {
+			udao.persistUser(u);
+			httpSession.setAttribute("user", u);
+			return ju.toJson(u);
+		} catch (Exception e) {
+			// We have to catch this exception ourselves, because we want to return json
+			// "null" when the user can't be persisted, but if an exception is thrown the
+			// logging aspect will take over and return actual null instead of json "null".
+			return ju.getJsonNull();
+		}
 	}
 
 	@RequestMapping(value = "/logout", method = RequestMethod.GET)
 	@ResponseBody
 	public String logout(HttpSession session) {
 		session.invalidate();
-		return JsonResult.getResult(true);
+		return ju.getJsonSuccess(true);
 	}
 }
