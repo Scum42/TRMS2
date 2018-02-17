b416e4b91ac51106c82f47d8c093b87a0a658b5a
Anthony Libardi <libardi42@gmail.com>
2018-02-17 14:36:45 -0500

    The autowiring in the test failed. Made the test class a @Component, does that help?

--- TRMS2/src/test/java/com/revature/data/UserDaoTest.java
+++ TRMS2/src/test/java/com/revature/data/UserDaoTest.java
@@ -3,7 +3,9 @@ package com.revature.data;
 import org.junit.Assert;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component
 public class UserDaoTest {
 	@Autowired
 	UserDao dao;
