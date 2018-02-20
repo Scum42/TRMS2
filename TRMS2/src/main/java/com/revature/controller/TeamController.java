6d451d45968d9c9e03ef9b34305f1f90a6cd2c29
Dillon Euell Pape <dillonpape@gmail.com>
2018-02-19 21:46:45 -0500

    Update TeamController to RestController, and getTeam() by ID

--- TRMS2/src/main/java/com/revature/controller/TeamController.java
+++ TRMS2/src/main/java/com/revature/controller/TeamController.java
@@ -1,26 +1,29 @@
 package com.revature.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.revature.beans.Team;
 import com.revature.services.TeamService;
-import com.revature.util.JsonUtil;
 
-@Controller
+@RestController
 @CrossOrigin(origins = { "http://localhost:4200", "http://18.216.71.226:4200" })
 public class TeamController {
 
 	@Autowired
 	private TeamService teamService;
 
-	@Autowired
-	private JsonUtil jsonUtil;
+	@RequestMapping(value = "/team", method = RequestMethod.GET)
+	public Team getTeam(HttpServletRequest request) throws JsonProcessingException {
+		int teamId = Integer.parseInt(request.getParameter("teamId"));
+		return teamService.getTeam(teamId);
+	}
 
 	/*
 	 * Test for all CRUD operations in TeamDaoHibernate/TeamService. Constructs a
@@ -28,31 +31,29 @@ public class TeamController {
 	 * database, updates the Team's name, and then deletes the Team from the
 	 * database.
 	 */
-	@RequestMapping(value = "/team", method = RequestMethod.GET)
-	@ResponseBody
-	public String getTeam() throws JsonProcessingException {
-		String response = "";
-
-		// Team team = new Team(0, "ChannelFireball Ice");
-		Team team = new Team();
-		team.setTeamName("ChannelFireball Ice");
-		response += "Our new team: " + jsonUtil.toJson(team) + "\n";
-
-		team = teamService.createTeam(team);
-		response += "Created team: " + jsonUtil.toJson(team) + "\n";
-
-		team = teamService.getTeam(team.getTeamId());
-		response += "Fetched team: " + jsonUtil.toJson(team) + "\n";
-
-		team.setTeamName("ChannelFireball Fire");
-		team = teamService.updateTeam(team);
-		response += "Updated team: " + jsonUtil.toJson(team) + "\n";
-
-		teamService.deleteTeam(team);
-		team = teamService.getTeam(team.getTeamId());
-		response += "Deleted team: " + jsonUtil.toJson(team) + "\n";
-
-		return response;
-	}
+	// public String getTeam() throws JsonProcessingException {
+	// String response = "";
+	//
+	// // Team team = new Team(0, "ChannelFireball Ice");
+	// Team team = new Team();
+	// team.setTeamName("ChannelFireball Ice");
+	// response += "Our new team: " + jsonUtil.toJson(team) + "\n";
+	//
+	// team = teamService.createTeam(team);
+	// response += "Created team: " + jsonUtil.toJson(team) + "\n";
+	//
+	// team = teamService.getTeam(team.getTeamId());
+	// response += "Fetched team: " + jsonUtil.toJson(team) + "\n";
+	//
+	// team.setTeamName("ChannelFireball Fire");
+	// team = teamService.updateTeam(team);
+	// response += "Updated team: " + jsonUtil.toJson(team) + "\n";
+	//
+	// teamService.deleteTeam(team);
+	// team = teamService.getTeam(team.getTeamId());
+	// response += "Deleted team: " + jsonUtil.toJson(team) + "\n";
+	//
+	// return response;
+	// }
 
 }
